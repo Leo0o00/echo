4ba47875b143183da6fdd3c478a72fc9c8676909
LeoOoOo <castillofernandezleonardo778@gmail.com>
2026-04-29 13:09:30 -0400

    Added organizations to user settings

---
+++ apps/web/app/(dashboard)/test/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Test = () => {
+  return <div>You only see this when you have created an organization</div>
+}
+
+export default Test
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google"
 
 import "@workspace/ui/globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@workspace/ui/lib/utils"
 import Providers from "@/components/providers"
 import { ClerkProvider } from "@clerk/nextjs"
@@ -31,9 +30,7 @@ export default function RootLayout({
     >
       <body>
         <ClerkProvider>
-          <Providers>
-            <ThemeProvider>{children}</ThemeProvider>
-          </Providers>
+          <Providers>{children}</Providers>
         </ClerkProvider>
       </body>
     </html>
