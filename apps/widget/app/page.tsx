77c7b99fd9154cd09d2e28791044c03cbbcb503f
LeoOoOo <castillofernandezleonardo778@gmail.com>
2026-04-30 13:10:53 -0400

    Added Vapi to widget app

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,30 @@
 "use client"
 
+import { useVapi } from "@/modules/widget/hooks/use-vapi"
 import { api } from "@workspace/backend/_generated/api"
+import { Button } from "@workspace/ui/components/button"
 import { useQuery } from "convex/react"
 
 export default function Page() {
-  const users = useQuery(api.users.getMany)
+  const {
+    isSpeaking,
+    isConnecting,
+    isConnected,
+    transcript,
+    startCall,
+    endCall,
+  } = useVapi()
 
   return (
-    <div className="flex min-h-svh items-center justify-center">
-      <p>apps/widget</p>
+    <div className="mx-auto flex min-h-svh w-full max-w-md flex-col items-center justify-center">
+      <Button onClick={() => startCall()}>Start Call</Button>
+      <Button onClick={() => endCall()} variant="destructive">
+        End Call
+      </Button>
+      <p>isConnected: {`${isConnected}`}</p>
+      <p>isConnecting: {`${isConnecting}`}</p>
+      <p>isSpeaking: {`${isSpeaking}`}</p>
+      <p>{JSON.stringify(transcript, null, 2)}</p>
     </div>
   )
 }
