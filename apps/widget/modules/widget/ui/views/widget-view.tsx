865d1921eb9e7f666027a2e9452f7495b3a5df68
LeoOoOo <castillofernandezleonardo778@gmail.com>
2026-05-03 11:57:59 -0400

    Added widget screens definitions

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,16 +1,30 @@
+import { useAtomValue } from "jotai"
 import { WidgetFooter } from "../components/widget-footer"
 import { WidgetHeader } from "../components/widget-header"
 import { WidgetAuthScreen } from "../screens/widget-auth-screen"
+import { screenAtom } from "../../atoms/widget-atoms"
 
 interface Props {
   organizationId: string
 }
 
 export const WidgetView = ({ organizationId }: Props) => {
+  const screen = useAtomValue(screenAtom)
+
+  const screenComponents = {
+    error: <p>TODO: Error</p>,
+    loading: <p>TODO: loading</p>,
+    auth: <WidgetAuthScreen />,
+    voice: <p>TODO: voice</p>,
+    inbox: <p>TODO: inbox</p>,
+    selection: <p>TODO: selection</p>,
+    chat: <p>TODO: chat</p>,
+    contact: <p>TODO: contact</p>,
+  }
+
   return (
     <main className="flex h-full min-h-screen w-full min-w-screen flex-col overflow-hidden rounded-xl border bg-muted">
-      <WidgetAuthScreen />
-      {/* <WidgetFooter /> */}
+      {screenComponents[screen]}
     </main>
   )
 }
