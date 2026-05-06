87fb431f58b35be7ab477b9c593d64f83093ee3d
LeoOoOo <castillofernandezleonardo778@gmail.com>
2026-05-06 09:22:48 -0400

    Added loading organization logic in widget

--- apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-auth-screen.tsx
@@ -13,6 +13,11 @@ import { Button } from "@workspace/ui/components/button"
 import { api } from "@workspace/backend/_generated/api"
 import { useMutation } from "convex/react"
 import { Doc } from "@workspace/backend/_generated/dataModel"
+import {
+  contactSessionIdAtomFamily,
+  organizationIdAtom,
+} from "../../atoms/widget-atoms"
+import { useAtomValue, useSetAtom } from "jotai"
 
 const formSchema = z.object({
   name: z.string().min(1, "Name is required"),
@@ -23,6 +28,11 @@ const formSchema = z.object({
 const organizationId = "123"
 
 export const WidgetAuthScreen = () => {
+  const organizationId = useAtomValue(organizationIdAtom)
+  const setContactSessionId = useSetAtom(
+    contactSessionIdAtomFamily(organizationId || "")
+  )
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -59,7 +69,7 @@ export const WidgetAuthScreen = () => {
       metadata,
     })
 
-    console.log({ contactSessionId })
+    setContactSessionId(contactSessionId)
   }
 
   return (
