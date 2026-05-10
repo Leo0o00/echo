8b5bc9db6165569a6d2f1b64295e3ad4fec4cc66
LeoOoOo <castillofernandezleonardo778@gmail.com>
2026-05-10 13:47:25 -0400

    Added widget inbox screen

--- apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
@@ -16,6 +16,7 @@ import {
 import { useMutation } from "convex/react"
 import { api } from "@workspace/backend/_generated/api"
 import { useState } from "react"
+import { WidgetFooter } from "../components/widget-footer"
 
 export const WidgetSelectionScreen = () => {
   const setScreen = useSetAtom(screenAtom)
@@ -80,6 +81,7 @@ export const WidgetSelectionScreen = () => {
           <ChevronRightIcon />
         </Button>
       </div>
+      <WidgetFooter />
     </>
   )
 }
